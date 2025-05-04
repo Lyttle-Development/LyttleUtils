5cdfe4e1081faba2d027d4874a3d832683a46ef2
stualyttle <stualyttle@users.noreply.github.com>
2025-05-04 20:48:01 +0200

    Use console correct

--- src/main/java/com/lyttledev/lyttleutils/LyttleUtils.java
+++ src/main/java/com/lyttledev/lyttleutils/LyttleUtils.java
@@ -6,10 +6,12 @@ import com.lyttledev.lyttleutils.utils.communication.Console;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class LyttleUtils extends JavaPlugin {
+    Console console;
+
     @Override
     public void onEnable() {
         // Print the plugin version
-        Console.init(this);
+        this.console = new Console(this);
         Console.log("LyttleUtils " + getDescription().getVersion() + " is enabled!");
     }
 }
